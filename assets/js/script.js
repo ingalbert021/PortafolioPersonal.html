960deb282f908830d0eb36f1ce1b689ae13c291b
alberto de la cruz alcantara <67446053+ingalbert021@users.noreply.github.com>
2020-10-28 11:46:21 -1200

    portafolio
    portafolio personal

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+"use strict"
+window.addEventListener("load", function (){
+
+/*Email funcion*/
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_55v008d';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      Swal.fire(
+  'Enviado correctamente!',
+  'Preciona!',
+  'success'
+)
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+
+  //efectos para texto
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+});
